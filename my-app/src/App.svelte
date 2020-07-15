094a017d9dd39dbe7849aa53ed65b2747bc01af9
Pablo Marino <pmarino2013@gmail.com>
2020-07-15 13:14:35 -0300

    Se dividio el proyecto en componentes

--- my-app/src/App.svelte
+++ my-app/src/App.svelte
@@ -1,4 +1,7 @@
 <script>
+import Lista from './components/Lista.svelte';
+import Recuadro from './components/Recuadro.svelte';
+
 //importo axios 
 import axios from 'axios';
 //importo del ciclo de vida el onMount
@@ -45,37 +48,16 @@ const cleanPersona=()=>{
 }
 	
 
+
 </script>
 
 <main>
 	<h1>Axios en Svelte</h1>
 	<h3>Haga clic en un nombre para ver mas detalles</h3>
-	<div class="lista">
-		<ul>
-		{#each datos as dato (dato.id)}
-			<li class="puntero" on:click={getUser(dato.id)}>
-				{dato.name}
-			</li>
-		{/each}
-		</ul>
-	</div>
-	{#if persona.length>0}
-	<section id="recuadro">
-	<div class="container">
-		<div class="boton">
-			<span class="btn-close" on:click={cleanPersona}></span>
-		
-		</div>
-		<section id="datos_persona"> 
-			<h3>{persona[0].name}</h3>
-			<span><strong>Direccion: </strong> {direccion.street} {direccion.suite}</span>
-		
-		</section>
-	</div>
-	</section>
-	{:else}
-		<h3>{loader ? 'Loading...': ''}</h3>
-	{/if}
+	
+	<Lista getUser={getUser} datos={datos} />
+
+	<Recuadro persona={persona} cleanPersona={cleanPersona} direccion={direccion} loader={loader} />
 </main>
 
 <style>
@@ -92,66 +74,7 @@ const cleanPersona=()=>{
 		font-size: 4em;
 		font-weight: 100;
 	}
-	.lista{
-		display: flex;
-		justify-content:center;
-	}
-	ul{
-		padding: 0;
-		margin: 10, 0;
-	}
-	.puntero{
-		cursor: pointer;
-		
-
-
-	}
-	.puntero:hover{
-		color:#ff3e00;
-	}
-	li{
-		list-style: none;
-	}
-	#recuadro{
-	    width: 100%;
-		display: flex;
-		justify-content: center;
-	}
-.container{
 	
-	width: 300px;
-	height: 150px;
-	text-align: center;
-	border:solid 1px burlywood;
-}
-.boton{
-	display: flex;
-	flex-direction: row-reverse;
-	height: 40px;
-	
-	background-color:black;
-	
-}
-.btn-close{
-	
-	width: 20px;
-	height: 20px;
-	margin: 10px;
-	
-	background-color: #ff3e00;
-	border-radius: 10px;
-	color:white;
-	border:0;
-	cursor: pointer;
-	
-}
-#datos_persona{
-	display: inline-block;
-width: 100%;
-	margin-top: 0;
-	padding-top: 0;
-
-}
 	@media (min-width: 640px) {
 		main {
 			max-width: none;
---
+++ my-app/src/components/Recuadro.svelte
@@ -0,0 +1,72 @@
+<script>
+export let persona;
+export let cleanPersona;
+export let loader;
+export let direccion;
+
+</script>
+
+<style>
+
+	#recuadro{
+	    width: 100%;
+		display: flex;
+		justify-content: center;
+	}
+    .container{
+        
+        width: 300px;
+        height: 150px;
+        text-align: center;
+        border:solid 1px burlywood;
+    }
+    .boton{
+        display: flex;
+        flex-direction: row-reverse;
+        height: 40px;
+        
+        background-color:black;
+        
+    }
+    .btn-close{
+        
+        width: 20px;
+        height: 20px;
+        margin: 10px;
+        
+        background-color: #ff3e00;
+        border-radius: 10px;
+        color:white;
+        border:0;
+        cursor: pointer;
+        
+    }
+    #datos_persona{
+        display: inline-block;
+    width: 100%;
+        margin-top: 0;
+        padding-top: 0;
+
+    }
+</style>
+
+<div class="Recuadro">
+{#if persona.length>0}
+	<section id="recuadro">
+	<div class="container">
+		<div class="boton">
+			<span class="btn-close" on:click={cleanPersona}></span>
+		
+		</div>
+		<section id="datos_persona"> 
+			<h3>{persona[0].name}</h3>
+			<span><strong>Direccion: </strong> {direccion.street} {direccion.suite}</span>
+		
+		</section>
+	</div>
+	</section>
+	{:else}
+		<h3>{loader ? 'Loading...': ''}</h3>
+	{/if}
+
+</div>
