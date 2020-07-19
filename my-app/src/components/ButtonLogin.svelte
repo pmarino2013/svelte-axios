fb17bd449a41442395623b4c7113aa51154d6842
Pablo Marino <pmarino2013@gmail.com>
2020-07-18 22:41:23 -0300

    Mejora de logueo con mensaje

--- my-app/src/components/ButtonLogin.svelte
+++ my-app/src/components/ButtonLogin.svelte
@@ -1,113 +1,132 @@
 <script>
-import {loginState} from '../store/store.js';
+  import { loginState } from "../store/store.js";
 
-import axios from 'axios';
+  import axios from "axios";
 
-let correoUser='';
-let usuario=[];
+  let correoUser = "";
+  let usuario = [];
+  let alerta = false;
 
-const validar= async (email)=>{
-	if($loginState){
-		loginState.set(false);
-	}else{
+  const validar = async email => {
+    if ($loginState) {
+      loginState.set(false);
+    } else {
+      const respuesta = await axios.get(
+        `https://jsonplaceholder.typicode.com/users?email=${email}`
+      );
 
-		const respuesta = await axios.get(`https://jsonplaceholder.typicode.com/users?email=${email}`)
-		
-		try {
-			
-			
-			usuario=respuesta.data;
-			console.log(usuario);
-			if(usuario.length>0){
-			loginState.set(true);
-			correoUser='';
-	
-			}
-					
-		
-		} catch (error) {
-			console.error(error)
-		}
-	}
+      try {
+        usuario = respuesta.data;
+        console.log(usuario);
+        if (usuario.length > 0) {
+          loginState.set(true);
+          correoUser = "";
+          alerta = false;
+        } else {
+          alerta = true;
+        }
+      } catch (error) {
+        console.error(error);
+      }
+    }
+  };
 
-}
-
-const setLogin=()=>{
-validar(correoUser);
-console.log(usuario)
-
-	
-}
+  const setLogin = () => {
+    validar(correoUser);
+    console.log(usuario);
+    console.log(alerta);
+  };
 </script>
 
 <style>
-.formulario{
-	display:inline-block;
+  .formulario {
+    display: inline-block;
+  }
+  .formulario input {
+    border-radius: 10px;
+    text-align: center;
+  }
 
-}
-.formulario input{
-	border-radius: 10px;
-	text-align: center;
-}
+  .btn-container {
+    margin-top: 10px;
+  }
 
-.btn-container{
-	margin-top: 10px;
-}
+  .btn-login {
+    width: 100px;
+    height: 40px;
+    border: none;
+    padding: 0 5px 5px 5px;
+    margin: 0;
+    background-color: #ff3e00;
+    color: white;
+    border-radius: 20px;
+    text-align: center;
+    display: inline-block;
+    cursor: pointer;
+  }
+  .btn-login:active {
+    background-color: #752003;
+    color: white;
+  }
+  .btn-logout {
+    width: 100px;
+    height: 40px;
+    border: none;
+    padding: 0 5px 5px 5px;
+    margin: 0;
+    background-color: dimgrey;
+    color: white;
+    border-radius: 20px;
+    text-align: center;
+    display: inline-block;
+    cursor: pointer;
+  }
+  .btn-logout:active {
+    background-color: rgb(58, 56, 56);
+    color: white;
+  }
 
-
-.btn-login{
-		width: 100px;
-		height: 40px;
-		border:none;
-		padding: 0 5px 5px 5px;
-		margin: 0;
-		background-color: #ff3e00;
-		color:white;
-		border-radius: 20px;
-		text-align: center;
-		display:inline-block;
-		cursor: pointer;
-	}
-	.btn-login:active{
-		background-color: #752003;
-		color:white;
-	}
-	.btn-logout{
-		width: 100px;
-		height: 40px;
-		border:none;
-		padding: 0 5px 5px 5px;
-		margin: 0;
-		background-color: dimgrey;
-		color:white;
-		border-radius: 20px;
-		text-align: center;
-		display:inline-block;
-		cursor: pointer;
-	
-	}
-	.btn-logout:active{
-		background-color: rgb(58, 56, 56);
-		color:white;
-	}
-	
+  .alert {
+    color: #721c24;
+    background-color: #f8d7da;
+    border-color: #f5c6cb;
+    padding: 10px;
+    border-radius: 0.25rem;
+  }
+  .mensaje {
+    margin-top: 1rem;
+    padding: 0.75rem 1.25rem;
+    margin-bottom: 1rem;
+    border: 1px solid transparent;
+    position: relative;
+  }
 </style>
 
-		
 <div class="ButtonLogin">
-	<form >
-	{#if !$loginState}
-		<div class="formulario" >
-		
-			<input type="text" placeholder="Ingrese su email" bind:value={correoUser}/>
+  <form>
+    {#if !$loginState}
+      <div class="formulario">
 
-		</div>
-	{/if}
-	<div class="btn-container">
-		<button type="button" class={$loginState ? 'btn-logout': 'btn-login'}  
-		on:click={validar(correoUser)}>{$loginState ? 'Logout':'Login'}
-		</button>
-	</div>
-	</form>
+        <input
+          type="text"
+          placeholder="Ingrese su email"
+          bind:value={correoUser} />
+
+      </div>
+    {/if}
+    <div class="btn-container">
+      <button
+        type="button"
+        class={$loginState ? 'btn-logout' : 'btn-login'}
+        on:click={validar(correoUser)}>
+        {$loginState ? 'Logout' : 'Login'}
+      </button>
+    </div>
+    {#if alerta}
+      <div class="mensaje">
+        <span class="alert">Usuario no registrado</span>
+
+      </div>
+    {/if}
+  </form>
 </div>
-
--- my-app/src/routes/Home.svelte
+++ my-app/src/routes/Home.svelte
@@ -1,105 +1,93 @@
 <script>
-import {loginState} from '../store/store.js';
+  import { loginState } from "../store/store.js";
 
-import Lista from '../components/Lista.svelte';
-import Recuadro from '../components/Recuadro.svelte';
-import ButtonLogin from '../components/ButtonLogin.svelte'
+  import Lista from "../components/Lista.svelte";
+  import Recuadro from "../components/Recuadro.svelte";
+  import ButtonLogin from "../components/ButtonLogin.svelte";
 
-//importando para usar bootstrap
+  //importando para usar bootstrap
 
+  import axios from "axios";
+  //importo del ciclo de vida el onMount
+  import { onMount } from "svelte";
 
-import axios from 'axios';
-//importo del ciclo de vida el onMount
-import {onMount} from 'svelte';
+  let datos = [];
+  let persona = {};
+  let loader = false;
+  let direccion = {};
+  const color = ["success"];
 
-	
-	let datos=[];
-	let persona={};
-	let loader=false;
-	let direccion={};
-	const color=[
-		"success"
+  //Cuando se monta el componente
+  onMount(() => {
+    getData();
+  });
 
-	];
-	
-	//Cuando se monta el componente
-	onMount(()=>{
-		getData();
-		
-	})
+  //función para traer la data
+  const getData = async () => {
+    const { data } = await axios.get(
+      "https://jsonplaceholder.typicode.com/users"
+    );
+    try {
+      datos = data;
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  //Función para consultar un usuario
+  const getUser = async user => {
+    persona = {};
+    loader = true;
+    const usuario = await axios.get(
+      `https://jsonplaceholder.typicode.com/users?id=${user}`
+    );
+    try {
+      persona = usuario.data;
+      direccion = persona[0].address;
 
-	//función para traer la data
-	const getData= async()=>{
-		const {data}= await axios.get('https://jsonplaceholder.typicode.com/users')
-		try {
-			datos=data;
-					
-		} catch (error) {
-			console.error(error)
-		}
-	}
-//Función para consultar un usuario
-const getUser=async (user)=>{
-	persona={};
-	loader=true;
-	const usuario = await axios.get(`https://jsonplaceholder.typicode.com/users?id=${user}`)
-	try {
-		persona=usuario.data;
-		direccion=persona[0].address;
-				
-		loader=false;
-	} catch (error) {
-		console.error(error)
-	}
-}
+      loader = false;
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
-const cleanPersona=()=>{
-	persona={};
-}
-	
-
+  const cleanPersona = () => {
+    persona = {};
+  };
 </script>
+
+<style>
+  .Home {
+    text-align: center;
+    padding: 1em;
+    max-width: 240px;
+    margin: 0 auto;
+  }
+
+  h1 {
+    color: #ff3e00;
+    text-transform: uppercase;
+    font-size: 4em;
+    font-weight: 100;
+  }
+
+  @media (min-width: 640px) {
+    .Home {
+      max-width: none;
+    }
+  }
+</style>
 
 <div class="Home">
 
-	<h1>Axios en Svelte</h1>
-	<ButtonLogin />
-	
-	{#if $loginState }
-	
-	<h3>Haga clic en un nombre para ver mas detalles</h3>
-	
-	<Lista {getUser} {datos} />
+  <h1>Axios en Svelte</h1>
+  <ButtonLogin />
 
-	<Recuadro {persona} {cleanPersona} {direccion} {loader} />
+  {#if $loginState}
+    <h3>Haga clic en un nombre para ver mas detalles</h3>
 
-	{:else}
-		<h3>No está logueado</h3>
-		
-	
-	{/if}
+    <Lista {getUser} {datos} />
+
+    <Recuadro {persona} {cleanPersona} {direccion} {loader} />
+  {/if}
 
 </div>
-
-<style>
-	.Home {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-	
-	@media (min-width: 640px) {
-		.Home {
-			max-width: none;
-		}
-	}
-</style>
